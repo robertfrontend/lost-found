36aa191a437842b75e90039c89e8e0a55c8cec91
Robert Romero <robert02frontend@gmail.com>
2024-07-22 13:44:15 -0400

    first commit

---
+++ src/app/components/layout/header.js
@@ -0,0 +1,8 @@
+const Header = () => (
+  <header className="text-center pt-10 pb-2">
+    <h1 className="text-3xl md:text-5xl font-semibold">Lost and Found Hub</h1>
+    <h4 className="text-lg mt-2">📍 Boston, MA</h4>
+  </header>
+);
+
+export default Header;
